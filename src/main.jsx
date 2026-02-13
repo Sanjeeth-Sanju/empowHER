349203602865c64489e037ca2dfb0b03a44dbd1b
Sanjeeth <sanjeeth022503@gmail.com>
2026-02-13 16:19:44 +0000

    Responsive css updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
+import "./css/landingPage.css";
+import "./css/navbar.css";
+import "./css/womensHealth.css";
+import "./css/healthTracker.css";
+import "./css/safety.css";
+import "./css/finance.css";
+import "./css/community.css";
+import "./css/education.css";
+import "./css/home.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
